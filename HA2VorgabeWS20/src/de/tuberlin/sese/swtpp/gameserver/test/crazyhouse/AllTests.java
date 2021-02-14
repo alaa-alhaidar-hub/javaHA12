009941807b100a9045c810c4752ebd147439137a
alaaalhaidar <alaa.alhaidar@hotmail.com>
2021-02-14 10:12:52 +0100

    SPLIT

---
+++ HA2VorgabeWS20/src/de/tuberlin/sese/swtpp/gameserver/test/crazyhouse/AllTests.java
@@ -0,0 +1,16 @@
+package de.tuberlin.sese.swtpp.gameserver.test.crazyhouse;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CrazyhouseGameTest.class, TryMoveIntegrationTest.class})
+public class AllTests {
+}
+
+
+//server
+//alhaidar
+//id 1983
+//psw alaa123
